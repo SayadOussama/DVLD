60678032dee770d69b29256b1cc508b73dd24a9e
Sayad Oussama <sayadoussamaseifeddine@gmail.com>
2024-12-04 22:31:36 +0100

    DVLD Application
    To Let the Program Work you need To Relate the the Parts together
     data Layer
     Business Layer
     Presentation Layer
    
    In Install the Data To you SQL Server

---
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = "Server=.;Database=DVLDDataBase;User Id=sa;Password=ddd123456789123;";
+
+
+    }
+}
